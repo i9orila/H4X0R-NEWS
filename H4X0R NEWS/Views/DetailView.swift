f35367b4abd294c90111b5cd663ac36bcf263127
I9orila <i9orila@aol.com>
2022-08-13 22:32:23 +0300

    fix

--- H4X0R NEWS/Views/DetailView.swift
+++ H4X0R NEWS/Views/DetailView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    let url: String?
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -15,6 +18,6 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(url: " ")
     }
 }
