c5deb7b55ad6109c5e13762cad4ee9740ca75303
I9orila <i9orila@aol.com>
2022-08-13 17:16:11 +0300

    add  Web View  skeleton

---
+++ H4X0R NEWS/Views/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  H4X0R NEWS
+//
+//  Created by I9orila T on 13.08.2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
